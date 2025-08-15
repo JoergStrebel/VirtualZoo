e01695b996e0a36e7e7b6fd1f7829f24d2537277
Jörg Strebel <joerg.strebel@posteo.de>
2025-08-15 18:03:57 +0200

    introducing  CGAL types

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,5 +1,2 @@
 #include "Point.h"
 
-Point::Point(const int px, const int py) : x(px), y(py)
-{
-}
--- src/Point.h
+++ src/Point.h
@@ -5,15 +5,23 @@
 /**
  * Geometric Point in the world's 2d plane (i.e. a pixel)
  */
-class Point
-{
-public:
-    const int x;
-    const int y;
-    Point(int px, int py);
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef Kernel::Point_2 CGAL_Point;
+
+class Point {
 private:
-        
+    CGAL_Point point;
+
+public:
+    Point(double x, double y) : point(x, y) {}
+
+    double getX() const { return point.x(); }
+    double getY() const { return point.y(); }
+
+    void setX(double x) { point = CGAL_Point(x, point.y()); }
+    void setY(double y) { point = CGAL_Point(point.x(), y); }
 };
 
 #endif // POINT_H
